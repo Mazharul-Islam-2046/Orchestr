56a5d742b75a8d39190da5843a43779f66be5d68
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2026-02-24 19:31:53 +0600

    chore(auth): Add the components for auth form

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from "@/components/ui/auth/register-form";
+
+
+export default function RegisterPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <RegisterForm />
+    </div>
+  )
+}
--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,8 +1,9 @@
+import { SignInForm } from "@/components/ui/auth/signin-form";
 
-const SignIn = () => {
-    return (
-        <div>SignIn</div>
-    );
-};
-
-export default SignIn;
+export default function Page() {
+  return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+              <SignInForm />
+            </div>
+  )
+}
