bb3b62427928f702a53bcde72f383cc41087607b
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2026-03-06 21:07:03 +0600

    config: Configure the global css and Add the redirection after signin and signup

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Button } from '../button';
+import { Button } from '../ui/button';
 import { signOutUser } from '@/actions/auth';
 
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { AuthError } from "next-auth";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const handleAuthError = (error: unknown) => {
+  if (error instanceof AuthError) {
+    switch (error.type) {
+      case "CredentialsSignin":
+        return { error: "Invalid credentials" };
+      default:
+        return { error: "Please confirm your email" };
+    }
+  }
+  throw error;
+};
