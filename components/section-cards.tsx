f0a99a713f2279a33adc339ede80e24d010e3525
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2026-05-22 11:20:36 +0600

    style: Style the basic structure of the navbar

--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -15,14 +15,14 @@ export function SectionCards() {
     <div className="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
+          <CardDescription>Total Appointments</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            $1,250.00
+            1,250
           </CardTitle>
           <CardAction>
             <Badge variant="outline">
               <IconTrendingUp />
-              +12.5%
+              +5%
             </Badge>
           </CardAction>
         </CardHeader>
@@ -31,7 +31,7 @@ export function SectionCards() {
             Trending up this month <IconTrendingUp className="size-4" />
           </div>
           <div className="text-muted-foreground">
-            Visitors for the last 6 months
+            Appoinmets for today: 150
           </div>
         </CardFooter>
       </Card>
